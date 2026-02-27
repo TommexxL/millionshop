c18063d5c67d89448481867811a2f5a8c379a384
TommexxL <tommexx_007@hotmail.com>
2026-02-27 16:00:42 +0100

    Fix title

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@ $cartCount = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Sanctified Thoughts & Prayers</title>
+<title>DigiPriest</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
