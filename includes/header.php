2a0372d1e4c844a71a394d5e372ef56b46b6dce8
TommexxL <tommexx_007@hotmail.com>
2026-02-27 16:51:19 +0100

    fixed links for assets

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,8 @@ $cartCount = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="/assets/styles.css" rel="stylesheet">
+<link href="assets/styles.css" rel="stylesheet">
+<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 </head>
 <body>
 
